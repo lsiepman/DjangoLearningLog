b44347e9bc66217fcbbe0d142fe1674cb768c4c2
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2024-02-23 19:49:52 +0100

    Made templates for additional pages. Learned about template inheritance

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     # Homepage
-    re_path(r"^$", views.index, name="index")
+    re_path(r"^$", views.index, name="index"),
+    # Show all topics
+    re_path(r"^topics/$", views.topics, name="topics"),
+    # Detail page for a single topic
+    re_path(r"^topics/(?P<topic_id>\d+)/$", views.topic, name="topic"),
 ]
