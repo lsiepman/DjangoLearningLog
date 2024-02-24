8dc2682d2ad55ecddb1252b0c22a7f0fc4a7e2d2
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2024-02-24 19:32:15 +0100

    Implemented log-in system

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(r"users/", include(("users.urls", "users"), namespace="users")),
     path(
         r"",
         include(("learning_logs.urls", "learning_logs"), namespace="learning_logs"),
---
+++ users/urls.py
@@ -0,0 +1,13 @@
+"Defines URL patterns for users"
+
+from django.urls import re_path
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+urlpatterns = [
+    # Login page
+    re_path(
+        r"^login/$", LoginView.as_view(template_name="users/login.html"), name="login"
+    )
+]
